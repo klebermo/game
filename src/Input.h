cdad7044200c62b49035d1ab9854e03041bb67b5
Kleber Mota de Oliveira <klebermo@gmail.com>
2022-03-23 19:08:55 -0300

    Update 23/03/2022 19:08

--- src/Input.h
+++ src/Input.h
@@ -1,6 +1,8 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+#include <SDL2/SDL.h>
+
 class Input {
 private:
   //
--- src/Shape.h
+++ src/Shape.h
@@ -1,9 +1,21 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
 class Shape {
 protected:
-  //
+  GLuint vao;
+  GLuint vbo;
+  GLuint vertexShader;
+  GLuint fragmentShader;
+  GLuint shaderProgram;
+
+  GLuint loadShader(GLuint type, const GLchar* shaderCode);
 public:
   Shape();
   ~Shape();
