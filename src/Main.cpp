f3ad8a9567fa7113e9c35c9e555e2353778a168b
Kleber Mota de Oliveira <klebermo@gmail.com>
2022-03-23 16:30:31 -0300

    Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,13 +4,9 @@ int main(int argc, char ** argv) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     return 0;
   } else {
-    cout << "1" << endl;
     Surface * surface = new Surface2d();
-    cout << "2" << endl;
     surface->setRenderer(Renderer());
-    cout << "3" << endl;
     surface->loop();
-    cout << "4" << endl;
   }
   SDL_Quit();
   return 1;
