cfa909f91e17cc8d3faf25295db458a637b74054
Kleber Mota de Oliveira <klebermo@gmail.com>
2022-04-17 21:02:13 -0300

    Update 17/04/2022 21:02

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,25 +5,56 @@
 
 #include "netpbm.h"
 
+#include <sstream>
+using namespace std;
+
 int main(int argc, char ** argv) {
   if (argc < 2) {
     cout << "Usage: game <file_name>" << endl;
     return 0;
   }
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    return 0;
-  } else {
-    Bitmap bitmap(argv[1]);
+  if (SDL_Init(SDL_INIT_VIDEO) >= 0) {
+    Netpbm * image;
 
-    int width = bitmap.getWidth(), height = bitmap.getHeight();
-    float * vertices = bitmap.toArray();
+    string file_name(argv[1]);
+    string extension;
+    stringstream ss(file_name);
+    while(getline(ss, extension, '.'));
 
-    Surface * view = new Surface2d("image", width, height);
-    view->setVertices(vertices);
-    view->loop();
+    int width = 0, height = 0;
+    float * vertices;
+
+    if(extension == "pbm") {
+      image = new Bitmap(argv[1]);
+      width = image->getWidth(), height = image->getHeight(), vertices = image->toArray();
+
+      Surface * view = new Surface2d("image", width, height);
+      view->setVertices(vertices);
+      view->loop();
+    }
+
+    if(extension == "pgm") {
+      image = new Graymap(argv[1]);
+      width = image->getWidth(), height = image->getHeight(), vertices = image->toArray();
+
+      Surface * view = new Surface2d("image", width, height);
+      view->setVertices(vertices);
+      view->loop();
+    }
+
+    if(extension == "ppm") {
+      image = new Pixmap(argv[1]);
+      width = image->getWidth(), height = image->getHeight(), vertices = image->toArray();
+
+      Surface * view = new Surface2d("image", width, height);
+      view->setVertices(vertices);
+      view->loop();
+    }
 
     delete vertices;
+    delete image;
+
     SDL_Quit();
   }
 
